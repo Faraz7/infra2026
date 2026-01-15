43314b0141e41ffa72e7e466aa35eeb6ecd04c61
Your Name <your-email@example.com>
2026-01-15 17:23:47 +0530

    project ID updated

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -1,5 +1,5 @@
 variable "project_id" {
-  default = "my-first-project"   # GCP project name
+  default = "project-3b16ca2c-2381-49c5-afc"   # GCP project ID
 }
 variable "region" {
   default = "us-central1"
